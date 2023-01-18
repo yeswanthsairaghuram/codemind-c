a5d7a62fca817cd216d06b26bfb7a8b5fb5cb8bd
yeswanthsairaghuram <119839691+yeswanthsairaghuram@users.noreply.github.com>
2023-01-18 18:44:01 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d" ,&x,&y,&z);     if(x>=y*30)     {         printf("YES" );     }     else     {         printf("NO" );     }
    }

---
+++ Expenditure.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    scanf("%d%d%d" ,&x,&y,&z);
+    if(x>=y*30)
+    {
+        printf("YES" ); 
+    } 
+    else 
+    {
+        printf("NO" );
+    }
+    
+}
